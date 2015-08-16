a313c370caffec19fc78878240691cd344d53240
nikolay.torbev <ntorbev@gmail.com>
2015-08-16 17:22:16 +0300

    handle proper cancel event

--- public/javascripts/account/ProfileCtrl.js
+++ public/javascripts/account/ProfileCtrl.js
@@ -20,5 +20,9 @@ app.controller('profileCtrl', function($scope, $location, auth, notifier, identi
         auth.update(user).then(function() {
             $location.path('/');
         });
+    };
+    $scope.cancel=function(user){
+        var socket = io().connect('http://localhost:8080');
+        socket.emit('newUser',{user:user.username});
     }
 });
